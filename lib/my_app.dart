cc6316ccacc3fdfbf2304cbfa4eebad6e8354c10
vitabinary <vitaassetbinary@gmail.com>
2021-03-22 12:23:52 +0700

    modified - comment

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+///Create parameter
 String selection;
 String inTemp;
 int temp;
@@ -9,12 +10,15 @@ dynamic tempConverter() {
     print(
         'Select temperature converter "A" convert celsius to fahrenheit and "B" convert fahrenheit to celsius');
     selection = stdin.readLineSync().toUpperCase();
-  } while (selection != 'A' && selection != 'B');
+  } while (selection != 'A' &&
+      selection !=
+          'B'); //Exit loop statement when program detact charater A OR B
 
   print('Enter the starting temperature: ');
-  inTemp = stdin.readLineSync();
-  temp = int.parse(inTemp);
+  inTemp = stdin.readLineSync(); // read keyboard.
+  temp = int.parse(inTemp); // convert parameter to integer
 
+  // Create contor flow statment that  switch case A and B.
   switch (selection) {
     case 'A':
       print('$temp degrees Celsius is ${temp * 1.8 + 32} degress Fahrenheit');
